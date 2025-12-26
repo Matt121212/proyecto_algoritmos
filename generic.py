aa3d474804f3e3df583465d2ea95dd97da966d8a
ivonnechauca10-spec <ivonnechauca10@gmail.com>
2025-12-26 17:39:36 -0500

    Fix image loading and window centering functions

--- generic.py
+++ generic.py
@@ -1,14 +1,18 @@
 from PIL import ImageTk, Image
 
 def leer_imagen(path, size):
-
-    return ImagentK.PhotoImagen(Imagen.open(path).rize(size, Image.Resampling.LANCZOS))
     
-def centrar_ventana(ventana,aplicacion_ancho, aplicacion_largo):
-    pantalla_largo = ventana.winfo_screenwidth()
-    pantalla_ancho = ventana.winfo_screenheight()
+    return ImageTk.PhotoImage(Image.open(path).resize(size, Image.Resampling.LANCZOS))
 
-    x = int ((pantalla_ancho/2) - (aplicacion_ancho/2))
-    y = int ((pantalla_largo/2) - (aplicacion_largo2))
+def centrar_ventana(ventana, aplicacion_ancho, aplicacion_largo):
+    pantalla_ancho = ventana.winfo_screenwidth()
+    pantalla_largo = ventana.winfo_screenheight()
     
+    x = int((pantalla_ancho/2) - (aplicacion_ancho/2))
+    y = int((pantalla_largo/2) - (aplicacion_largo/2))
+    
+
     return ventana.geometry(f"{aplicacion_ancho}x{aplicacion_largo}+{x}+{y}")
+
+
+
